c068103f796c656b7b8bcbecbd0d3f057f51006b
Emma With <emma.m.with@gmail.com>
2024-02-13 11:48:31 +0100

    emmas branch

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,1 +1,30 @@
-<template><h1>About</h1></template>
+<template>
+  <h1>Om</h1>
+  <div class="parallax"></div>
+  <div id="second"></div>
+</template>
+
+<style>
+.parallax {
+  border: 3px solid black;
+  width: 100%;
+  height: 50em;
+  background-image: url("../assets/leaf.png");
+
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+#second {
+  border: 3px solid black;
+  width: 100%;
+  height: 50em;
+  background-image: url("../assets/landscape.jpeg");
+
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
