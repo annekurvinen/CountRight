92eb675e4ab7765ee2f020ec41bb7ae55a8b556f
Emma With <emma.m.with@gmail.com>
2024-02-15 15:53:41 +0100

    emma

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 import HeaderLogo from "./components/HeaderLogo.vue";
 
 export default {
@@ -15,16 +14,3 @@ export default {
   },
 };
 </script>
-
-<style scoped></style>
-=======
-import HeaderLogo from './components/HeaderLogo.vue';
-import LoginForm from './components/LoginForm.vue';
-export default {
-  components: {
-    HeaderLogo,
-    LoginForm,
-  },
-};
-</script>
->>>>>>> 1259d65bf500c46eba7d4d13b5ff4357696427ab
