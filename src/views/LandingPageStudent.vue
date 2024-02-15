138cdf930b0ca9dc459359141bd63e4360c35dc5
Emma With <emma.m.with@gmail.com>
2024-02-15 16:50:12 +0100

    ändrat student ladingpage

--- src/views/LandingPageStudent.vue
+++ src/views/LandingPageStudent.vue
@@ -1,6 +1,25 @@
 <template>
-  <h1>ladingpage STUDENT</h1>
-  <main></main>
+  <h1>Hej Alice</h1>
+  <main id="studentButton">
+    <b-button variant="warning">Starta prov</b-button>
+    <b-button variant="warning">Se tidigare prov</b-button>
+    <b-button variant="warning">Ändra lösenord </b-button>
+  </main>
 </template>
 
 <script></script>
+<style>
+h1 {
+  display: flex;
+  justify-content: center;
+}
+#studentButton {
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+b-button {
+  margin: 10rem;
+}
+</style>
