f26b85d8ba82225ca300ef9ae0ebec1f662d2a55
Abdiaziz Oog <abdoog0313@hotmail.com>
2024-02-15 14:02:45 +0100

    login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -33,3 +33,54 @@ export default {
     }
 }
 </script>
+<style scoped>
+.login-container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .form-container {
+    width: 90vw; /* Anpassa beroende på önskad bredd */
+    padding: 20px; /* Justera beroende på önskat avstånd från sidokanter */
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+  }
+
+  .input-field {
+    width: 100%;
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  .checkbox-container {
+    margin-bottom: 10px;
+  }
+
+  .checkbox-container label {
+    display: block;
+    margin-bottom: 5px;
+    font-size: 16px;
+  }
+
+  .submit-button {
+    width: 100%;
+    padding: 10px;
+    background-color: #007bff;
+    border: none;
+    border-radius: 3px;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  .submit-button:hover {
+    background-color: #0056b3;
+  }
+</style>
