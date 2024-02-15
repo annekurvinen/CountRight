07191d96e196d432473caad70699812ea873a70b
kotapis <sofia.l.levander@gmail.com>
2024-02-15 16:04:31 +0100

    lagt till färgvariabel

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import HeaderLogo from "./components/HeaderLogo.vue";
+import HeaderLogo from './components/HeaderLogo.vue';
 
 export default {
   components: {
@@ -17,14 +16,3 @@ export default {
 </script>
 
 <style scoped></style>
-=======
-import HeaderLogo from './components/HeaderLogo.vue';
-import LoginForm from './components/LoginForm.vue';
-export default {
-  components: {
-    HeaderLogo,
-    LoginForm,
-  },
-};
-</script>
->>>>>>> 1259d65bf500c46eba7d4d13b5ff4357696427ab
