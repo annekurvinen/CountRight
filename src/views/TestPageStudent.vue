59fd055ac39a74ecaf03b4e0327a02c4b9dca57b
kotapis <sofia.l.levander@gmail.com>
2024-02-16 12:35:38 +0100

    kommenterat ut mina tankar

--- src/views/TestPageStudent.vue
+++ src/views/TestPageStudent.vue
@@ -2,28 +2,38 @@
   <div id="question">
     <h1>Välkommen till dagens prov</h1>
     <div>
-      <section v-for="(question, index) in combinedQuestions" :key="index">{{ question.fråga }}</section>
+      <!-- <section v-for="(question, index) in combinedQuestions" :key="index">{{ question.fråga }}</section> -->
+      <question></question>
     </div>
   </div>
 </template>
 
 <script>
-import questionData from "../JSON/questions.json"
+// for (let n=1; n < 13; n++) {
+//   question
+// question number = n
+// if (n<13) {
+//   next question
+// } else if (n=13) {
+//   test result
+// }
+// }
+
+import questionData from '../JSON/questions.json';
 
 export default {
   data() {
     return {
-      combinedQuestions: questionData.algebra
-    }
-  }
-}
+      combinedQuestions: questionData.algebra,
+    };
+  },
+};
 </script>
 
 <style>
 #question {
-display: flex;
-flex-direction: column;
-align-items: center;
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
