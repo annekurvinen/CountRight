0d2526fb3aec8554322bb4ad1fa18eb7386f32ad
kotapis <sofia.l.levander@gmail.com>
2024-02-16 12:47:22 +0100

    återställt testpagestudent

--- src/views/TestPageStudent.vue
+++ src/views/TestPageStudent.vue
@@ -2,23 +2,14 @@
   <div id="question">
     <h1>Välkommen till dagens prov</h1>
     <div>
-      <!-- <section v-for="(question, index) in combinedQuestions" :key="index">{{ question.fråga }}</section> -->
-      <question></question>
+      <section v-for="(question, index) in combinedQuestions" :key="index">
+        {{ question.fråga }}
+      </section>
     </div>
   </div>
 </template>
 
 <script>
-// for (let n=1; n < 13; n++) {
-//   question
-// question number = n
-// if (n<13) {
-//   next question
-// } else if (n=13) {
-//   test result
-// }
-// }
-
 import questionData from '../JSON/questions.json';
 
 export default {
