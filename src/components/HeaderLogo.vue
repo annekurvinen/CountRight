764ad1e2b4d40763039809103a4add6aef53edab
kotapis <sofia.l.levander@gmail.com>
2024-02-19 15:01:36 +0100

    arbete med header

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -19,7 +19,7 @@
   align-items: center;
   justify-content: flex-start;
   gap: 6vw;
-  margin-left: 59px;
+  margin-left: 4rem;
 }
 
 h1 {
@@ -28,7 +28,7 @@ h1 {
 
 img {
   border-radius: 100%;
-  height: 7rem;
+  height: 14rem;
   width: auto;
 }
 
@@ -45,10 +45,10 @@ header {
 
 /*style för tablett och desktop  */
 @media only screen and (min-width: 600px) {
-  /* h1 { */
-  /* margin-top: 7rem;
-    font-size: 3rem; */
-  /* } */
+  h1 {
+    margin-top: 7rem;
+    font-size: 3rem;
+  }
 }
 /* @media only screen and (min-width: 600px) {
   h1 {
@@ -56,3 +56,60 @@ header {
   }
 } Nödvändigt attt ha detta? */
 </style>
+
+<!--
+<template>
+  <header>
+    <img src="../assets/Logo.jpg" alt="logo" />
+    <h1>Räkna Lätt</h1>
+  </header>
+</template>
+
+<script></script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+header {
+  display: flex;
+}
+h1 {
+  margin-top: 2rem;
+  font-family: "Expletus Sans", sans-serif;
+}
+
+img {
+  border-radius: 100%;
+  height: auto;
+  width: 7rem;
+  margin: 0.2rem;
+}
+header {
+  background: linear-gradient(
+    180deg,
+    rgba(220, 165, 114, 1) 0%,
+    rgba(220, 165, 114, 0.41) 72%,
+    rgba(220, 165, 114, 0) 100%
+  );
+}
+
+/*style för tablett och desktop  */
+@media only screen and (min-width: 600px) {
+  img {
+    border-radius: 100%;
+    height: auto;
+    width: 15rem;
+    margin: 1rem;
+  }
+  h1 {
+    margin-top: 7rem;
+    font-size: 3rem;
+  }
+}
+/* @media only screen and (min-width: 600px) {
+  h1 {
+    margin-top: 2rem;
+  }
+} Nödvändigt attt ha detta? */
+</style> -->
