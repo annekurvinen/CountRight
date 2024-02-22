ec2ae56d26b34525be80234f9361cedcc0c0f016
Abdiaziz Oog <abdoog0313@hotmail.com>
2024-02-22 11:24:42 +0100

    ny komponent Logout

---
+++ src/components/Logout.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    onOk() {
+      console.log('Ok')
+    }
+  }
+}
+
+</script>
+
+<template>
+  <BButton @click="showModal = !showModal" variant="primary">
+    Logga ut
+  </BButton>
+  <BModal @ok="onOk" v-model="showModal" title="Godkänna">
+    Är du säker att du vill logga ut?
+  </BModal>
+</template>
