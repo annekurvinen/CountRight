c4e149b04b4a01162a39ac2712baf3a561e24d5d
Elma <vilaelma1@gmail.com>
2024-02-26 15:45:40 +0100

    rullgardinsmeny

--- src/views/TestPageTeacher.vue
+++ src/views/TestPageTeacher.vue
@@ -1,14 +1,34 @@
 <template>
   <nav-bar />
-  <h1>inne i provsidan</h1>
-  <main />
+  <!-- <div>
+    <h1>Skapa prov</h1>
+    <main />
+    <select v-model="selectedCategory">
+      <option disabled selected hidden>Välj kategori</option>
+      <option value="0">Algebra</option>
+      <option value="1">Statistik</option>
+      Lägg till fler kategorier efter behov
+    </select>
+  </div>-->
+  <div>
+  <b-dropdown id="dropdown-1" text="Välj Kategori" class="m-md-2">
+    <b-dropdown-item @click="selectedCategory ">Algebra</b-dropdown-item>
+    <b-dropdown-item @click="selectedCategory ">Statistik</b-dropdown-item>
+  </b-dropdown>
+</div>
 </template>
 
 <script>
-import NavBar from "../components/NavBar.vue";
-export default {
-  components: {
-    NavBar,
-  },
-};
+  import NavBar from '../components/NavBar.vue'
+  export default {
+    components: {
+      NavBar
+    }
+  }
 </script>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
